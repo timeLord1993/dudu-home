a00fbdd753f26caf16815ee4a3b48078ce0a28f6
张叁疯 <zhangyun7@dgg.net>
2023-01-17 14:58:56 +0800

    feat: markdown编辑器添加

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,12 +1,21 @@
 <template>
   <div>博客</div>
+  <md-editor v-model="state.text" :theme="state.theme" />
 </template>
-<script>
+<script lang="ts">
 export default defineComponent({
-  name: "Blog",
-  components: {}
+  name: "Blog"
 });
 </script>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { reactive } from "vue";
+import MdEditor from "md-editor-v3";
+import "md-editor-v3/lib/style.css";
+
+const state = reactive({
+  text: "hello world",
+  theme: "dark"
+});
+</script>
 
 <style scoped></style>
